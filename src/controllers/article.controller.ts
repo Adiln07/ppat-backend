280e3a1a8ce524c1657efb0c7556411349e70a7f
Muhammad Adil <adil.nusabakti117@gmail.com>
2026-08-14 17:00:56 +0800

    bug error

--- src/controllers/article.controller.ts
+++ src/controllers/article.controller.ts
@@ -1,3 +1,108 @@
+// import {
+//   getAllArticleUseCase,
+//   getArticleByIdUseCase,
+//   createArticleUseCase,
+//   updateArticleUseCase,
+//   deletedArticleUseCase,
+// } from "../usecases/article.usecase.js";
+// import { Request, Response } from "express";
+// import { successResponse, errorResponse } from "../utils/response.js";
+// import { ArticleInput, FilterArticle } from "../types/article.js";
+
+// const getAllArticleController = async (req: Request, res: Response) => {
+//   try {
+//     const filter: FilterArticle = {
+//       title: req.query.name ? String(req.query.name) : undefined,
+//       page: req.query.pages ? Number(req.query.pages) : undefined,
+//       limit: req.query.limit ? Number(req.query.limit) : undefined,
+//     };
+
+//     const { articles, pagination } = await getAllArticleUseCase(filter);
+
+//     successResponse(res, articles, "Articles fetched successfully", pagination);
+//   } catch (error) {
+//     const errorMessage =
+//       error instanceof Error ? error.message : "Failed to fetch articles";
+
+//     errorResponse(res, errorMessage);
+//   }
+// };
+
+// const getArticleByIdController = async (req: Request, res: Response) => {
+//   try {
+//     const articleId = Number(req.params.id);
+//     const article = await getArticleByIdUseCase(articleId);
+//     successResponse(res, article, "Artcile By Id fetched successfully");
+//   } catch (error) {
+//     const errorMessage =
+//       error instanceof Error ? error.message : "Failed to fetch notary";
+//     errorResponse(res, errorMessage);
+//   }
+// };
+
+// const createArticleController = async (req: Request, res: Response) => {
+//   try {
+//     const { title, description, eventDate, theme, imageUrl }: ArticleInput =
+//       req.body;
+
+//     const newArticleData = { title, description, eventDate, theme, imageUrl };
+//     const newArticle = await createArticleUseCase(newArticleData);
+//     successResponse(res, newArticle, "create Article is Success");
+//   } catch (error) {
+//     const errorMessage =
+//       error instanceof Error ? error.message : "Failed to create article";
+//     errorResponse(res, errorMessage);
+//   }
+// };
+
+// const updateArticleController = async (req: Request, res: Response) => {
+//   try {
+//     const articleId = Number(req.params.id);
+//     const { title, description, eventDate, theme, imageUrl }: ArticleInput =
+//       req.body;
+
+//     const updateArticleData = {
+//       title,
+//       description,
+//       eventDate,
+//       theme,
+//       imageUrl,
+//     };
+//     const updateArticle = await updateArticleUseCase(
+//       articleId,
+//       updateArticleData,
+//     );
+
+//     successResponse(res, updateArticle, "Update Article is Success");
+//   } catch (error) {
+//     const errorMessage =
+//       error instanceof Error ? error.message : "Failed to update article";
+//     errorResponse(res, errorMessage);
+//   }
+// };
+
+// const deleteArticleController = async (req: Request, res: Response) => {
+//   try {
+//     const articleId = Number(req.params.id);
+
+//     const deletedArticle = await deletedArticleUseCase(articleId);
+
+//     successResponse(res, deletedArticle, "Delter Article is Success");
+//   } catch (error) {
+//     const errorMessage =
+//       error instanceof Error ? error.message : "Failed to delete article";
+//     errorResponse(res, errorMessage);
+//   }
+// };
+
+// export {
+//   getAllArticleController,
+//   getArticleByIdController,
+//   createArticleController,
+//   updateArticleController,
+//   deleteArticleController,
+// };
+
 import {
   getAllArticleUseCase,
   getArticleByIdUseCase,
@@ -21,6 +126,8 @@ const getAllArticleController = async (req: Request, res: Response) => {
 
     successResponse(res, articles, "Articles fetched successfully", pagination);
   } catch (error) {
+    console.error("DEBUG ERROR:", error);
+
     const errorMessage =
       error instanceof Error ? error.message : "Failed to fetch articles";
 
